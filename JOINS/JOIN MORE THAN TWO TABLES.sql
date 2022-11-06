ad3885cc28b4788a4b88973157dd2875200e6e24
ankandatta1601 <110905082+ankandatta1601@users.noreply.github.com>
2022-11-06 21:24:28 +0530

    Add files via upload

---
+++ JOINS/JOIN MORE THAN TWO TABLES.sql
@@ -0,0 +1,30 @@
+SELECT 
+    e.first_name,
+    e.last_name,
+    e.hire_date,
+    dm.from_date,
+    d.dept_name
+FROM
+    employees e
+        JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no
+        JOIN
+    departments d ON d.dept_no = dm.dept_no;
+    
+SELECT 
+    e.first_name,
+    e.last_name,
+    e.hire_date,
+    t.title,
+    m.from_date,
+    d.dept_name
+FROM
+    employees e
+        JOIN
+    dept_manager m ON e.emp_no = m.emp_no
+        JOIN
+    departments d ON m.dept_no = d.dept_no
+        JOIN
+    titles t ON e.emp_no = t.emp_no
+        AND m.from_date = t.from_date
+ORDER BY e.emp_no;
---
+++ JOINS/JOIN WITH AGGREGATE FUNCTION.sql
@@ -0,0 +1,7 @@
+SELECT 
+    e.gender, AVG(s.salary) AS avg_salary
+FROM
+    employees e
+        JOIN
+    salaries s ON e.emp_no = s.emp_no
+GROUP BY gender;
